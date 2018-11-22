4e851f5d3048b00e4a8cca2b0dfb9381a43f4bea
Matej Razingar <matejrazingar@yahoo.com>
2018-11-22 12:43:18 +0100

    Added labeling of holes and objects

---
+++ CMakeFiles/Pick_and_place.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pick_and_place.dir/pickandplace.cpp.o"
+  "Pick_and_place.pdb"
+  "Pick_and_place"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pick_and_place.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
